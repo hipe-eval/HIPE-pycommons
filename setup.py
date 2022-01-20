7c16681b620be375475d13f9da8fab0995abcea7
Matteo Romanello <matteo.romanello@epfl.ch>
2022-01-20 16:36:57 +0100

    wip + added a basic setup.py file for pip-based installation

---
+++ setup.py
@@ -0,0 +1,24 @@
+import os
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="hipe_commons",
+    url="https://github.com/hipe-eval/HIPE-pycommons",
+    version="0.1.0",
+    packages=find_packages(),
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: POSIX",
+    ],
+    license="GPL v3"
+    ,
+    install_requires=[],
+    long_description=read("README.md"),
+    long_description_content_type='text/markdown'
+)
