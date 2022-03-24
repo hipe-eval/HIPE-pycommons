6bbcdcb210e1f2ee7f9b6cf79b9fb99894997229
Matteo Romanello <matteo.romanello@epfl.ch>
2022-03-24 16:04:32 +0100

    adding one test for tsv helpers

--- tests/test_helpers_tsv.py
+++ tests/test_helpers_tsv.py
@@ -1,4 +1,4 @@
-import ipdb
+import os
 from hipe_commons.helpers.tsv import parse_tsv, HipeDocument
 
 def test_parse_tsv_from_file(ajmc_en_sample_path):
@@ -8,6 +8,14 @@ def test_parse_tsv_from_file(ajmc_en_sample_path):
     assert len(docs) > 0
     assert all([isinstance(doc, HipeDocument) for doc in docs])
 
-# TODO
 def test_parse_tsv_from_url():
-    pass
+    
+    tsv_file_url = os.path.join(
+        "https://raw.githubusercontent.com/impresso/CLEF-HIPE-2020-eval/",
+        "master/data/release/v1.3/fr/HIPE-data-v1.3-test-fr.tsv"
+    )
+
+    docs = parse_tsv(file_url=tsv_file_url)
+
+    assert len(docs) > 0
+    assert all([isinstance(doc, HipeDocument) for doc in docs])
